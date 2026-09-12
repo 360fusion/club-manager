18d0aa58c74c45d3507d5deb9753f53dbba12ed1
Ben Kenyon <ben@Samanthas-MacBook-Air.local>
2026-09-12 23:48:19 +0100

    Fix Vite asset tag directive in app.blade.php and clear caches

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>{{ config('app.name', 'Laravel') }}</title>
         @routes
-        @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
         @inertiaHead
     </head>
     <body class="font-sans antialiased">
